7261e6c2daa22251b8ff66889fec21fe1c79a17a
Fantasm4no <erickzhigue@gmail.com>
2024-10-21 12:38:26 -0500

    21/10

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -17,5 +17,48 @@ public class Controller {
         this.searchMethods = searchMethods; //Si el nombre fuese diferente, no iria el this view = vista; 
         System.out.println("Controller created");
     }
+
+    public void start(){
+        int option = 0;
+        do{
+            option = view.showMenu();
+            switch(option){
+                case 1:
+                    inputPersons();
+                    break;
+                case 2:
+                    addPerson();
+                    break;
+                case 100:
+                    System.out.println("Adios");
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+        while(option != 10);
+        
+    }
     
+    public void inputPersons(){
+        int numeroPersonas = view.inputInt("Ingrese el numero de personas: ");
+        personas = new Person[numeroPersonas];
+        for(int i=0; i < numeroPersonas; i++){
+            personas[i] = view.inputPerson();
+        }
+    }
+
+    public void addPerson(){
+        int numeroPersonas = view.inputInt("Ingrese el numero de personas a adicionar: ");
+        Person [] personasTotales = new Person[personas.length + numeroPersonas];
+
+        for (int i = 0; i < personas.length; i++){
+            personasTotales[i] = personas[i];
+        }
+
+        for (int i = personas.length; i < personasTotales.length; i++){
+            personasTotales[i] = view.inputPerson();
+        }
+
+        personas = personasTotales;
+    }
 }
--- src/views/View.java
+++ src/views/View.java
@@ -1,5 +1,42 @@
 package views;
+import java.util.Scanner;
+import models.Person;
 
 public class View {
+    private Scanner scanner;
+
+    public View(){
+        scanner = new Scanner(System.in);
+    }
+
+    public int showMenu(){
+        System.out.println("*****MENU******");
+        System.out.println("1. Ingresar personas");
+        System.out.println("2. Adicionar personas");
+        System.out.println("100. Salir");
+        return scanner.nextInt();
+
+    }
+
+    public int inputInt(String message){
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public Person inputPerson(){
+        String name = inputName();
+        int age = inputAge();
+        return new Person(name, age);
+    }
+    
+    public String inputName(){
+        System.out.println("Ingrese el nombre: ");
+        return scanner.next();
+    }
+
+    public int inputAge(){
+        return inputInt("Ingrese la edad: ");
+    }
+
     
 }
