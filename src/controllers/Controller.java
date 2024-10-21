8b8e834b60c728d03e9c43112dac231b5d5d39cc
Fantasm4no <erickzhigue@gmail.com>
2024-10-21 11:31:18 -0500

    21/10

---
+++ src/controllers/Controller.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import models.Person;
+import views.View;
+
+public class Controller {
+    private View view;
+    private SortingMethods sortingMethods;
+    private SearchMethods searchMethods;
+
+    private Person[] personas;
+
+    public Controller(View view, SortingMethods sortingMethods, SearchMethods searchMethods){
+
+    }
+    
+}
