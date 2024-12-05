cbc191bd62f5e673a79357a0f78048e80b2b7ed3
mvcobos <vcobos178@gmail.com>
2024-12-04 22:46:27 -0500

    Update SortingMethods.java

--- src/controllers/SortingMethods.java
+++ src/controllers/SortingMethods.java
@@ -4,37 +4,54 @@ import models.Person;
 
 public class SortingMethods {
 
-    public Person binarySearchByAge(Person[] people, int age) {
-        int left = 0, right = people.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (people[mid].getAge() == age) {
-                return people[mid];
-            }
-            if (people[mid].getAge() < age) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+    public void sortByNameWithBubble(Person[] people) {
+        for (int i = 0; i < people.length - 1; i++) {
+            for (int j = 0; j < people.length - i - 1; j++) {
+                if (people[j].getName().compareTo(people[j + 1].getName()) > 0) {
+                    Person temp = people[j];
+                    people[j] = people[j + 1];
+                    people[j + 1] = temp;
+                }
             }
         }
-        return null;
     }
     
-    public Person binarySearchByName(Person[] people, String name) {
-        int left = 0, right = people.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int comparison = people[mid].getName().compareToIgnoreCase(name);
-            if (comparison == 0) {
-                return people[mid];
+    public void sortByNameWithSelectionDesc(Person[] people) {
+        for (int i = 0; i < people.length - 1; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < people.length; j++) {
+                if (people[j].getName().compareTo(people[maxIdx].getName()) > 0) {
+                    maxIdx = j;
+                }
             }
-            if (comparison < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            Person temp = people[maxIdx];
+            people[maxIdx] = people[i];
+            people[i] = temp;
         }
-        return null;
+    }
+    
+    public void sortByAgeWithInsertion(Person[] people) {
+        for (int i = 1; i < people.length; i++) {
+            Person key = people[i];
+            int j = i - 1;
+            while (j >= 0 && people[j].getAge() > key.getAge()) {
+                people[j + 1] = people[j];
+                j--;
+            }
+            people[j + 1] = key;
+        }
+    }
+    
+    public void sortByNameWithInsertion(Person[] people) {
+        for (int i = 1; i < people.length; i++) {
+            Person key = people[i];
+            int j = i - 1;
+            while (j >= 0 && people[j].getName().compareTo(key.getName()) > 0) {
+                people[j + 1] = people[j];
+                j--;
+            }
+            people[j + 1] = key;
+        }
     }
 
 }
