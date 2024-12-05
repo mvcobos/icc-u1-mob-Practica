4cb23edfc0a0764e20b048b485a21d10dc55c8dc
mvcobos <vcobos178@gmail.com>
2024-12-04 22:32:59 -0500

    Update Controller.java

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -5,79 +5,106 @@ import views.View;
 
 public class Controller {
     private View view;
+    private Person[] people;
     private SortingMethods sortingMethods;
     private SearchMethods searchMethods;
 
-    private Person[] personas;
-
-    public Controller(View view, SortingMethods sortingMethods, SearchMethods searchMethods){
-        //This es la instancia de la clase en la propia clase = controller.view = view;
+    public Controller(View view) {
         this.view = view;
-        this.sortingMethods = sortingMethods;
-        this.searchMethods = searchMethods; //Si el nombre fuese diferente, no iria el this view = vista; 
-        System.out.println("Controller created");
+        this.sortingMethods = new SortingMethods();
+        this.searchMethods = new SearchMethods();
     }
 
-    public void start(){
-        int option = 0;
-        do{
+    public void start() {
+        int option;
+        do {
             option = view.showMenu();
-            switch(option){
+            switch (option) {
                 case 1:
-                    inputPersons();
+                    inputPeople();
                     break;
                 case 2:
                     addPerson();
                     break;
                 case 3:
-                    sortPersons();
-                case 100:
-                    System.out.println("Adios");
+                    sortPeople();
+                    break;
+                case 4:
+                    searchPerson();
+                    break;
                 default:
-                    System.out.println("Opcion no valida");
+                    view.showMessage("Opción no válida.");
             }
-        }
-        while(option != 10);
-        
+        } while (option != 0);
     }
-    
-    public void sortPersons() {
-        int sortingOption = view.selectSortingMethod();
 
-        if(sortingOption == 1){
-            sortingMethods.sortByNameBubble(personas);
-        }else if(sortingOption == 2){
-            sortingMethods.sortByAgeWhitSelection(personas);
-        } else {
-            view.showMessage("Opcion invalida");
+    private void inputPeople() {
+        int count = view.inputInt("Ingrese el número de personas: ");
+        people = new Person[count];
+        for (int i = 0; i < count; i++) {
+            people[i] = view.inputPerson();
         }
     }
 
-    public void inputPersons(){
-        int numeroPersonas = view.inputInt("Ingrese el numero de personas: ");
-        personas = new Person[numeroPersonas];
-        for(int i=0; i < numeroPersonas; i++){
-            personas[i] = view.inputPerson();
+    private void addPerson() {
+        if (people == null) {
+            view.showMessage("Primero debe ingresar personas.");
+            inputPeople();
+            return;
         }
+        int newCount = view.inputInt("Ingrese cuántas personas desea agregar: ");
+        Person[] newPeople = new Person[people.length + newCount];
+        System.arraycopy(people, 0, newPeople, 0, people.length);
+        for (int i = people.length; i < newPeople.length; i++) {
+            newPeople[i] = view.inputPerson();
+        }
+        people = newPeople;
     }
 
-    public void addPerson(){
-        if(personas == null){
-            view.showMessage("No existe, ingrese las personas por primera vez");
-            inputPersons();
-        } else{
-            int numeroPersonas = view.inputInt("Ingrese el numero de personas a adicionar: ");
-            Person [] personasTotales = new Person[personas.length + numeroPersonas];
-    
-            for (int i = 0; i < personas.length; i++){
-                personasTotales[i] = personas[i];
-            }
-    
-            for (int i = personas.length; i < personasTotales.length; i++){
-                personasTotales[i] = view.inputPerson();
-            }
-    
-            personas = personasTotales;
+    private void sortPeople() {
+        if (people == null || people.length == 0) {
+            view.showMessage("No hay personas para ordenar.");
+            return;
+        }
+        int sortOption = view.selectSortingMethod();
+        switch (sortOption) {
+            case 1:
+                sortingMethods.sortByNameWithBubble(people);
+                break;
+            case 2:
+                sortingMethods.sortByNameWithSelectionDesc(people);
+                break;
+            case 3:
+                sortingMethods.sortByAgeWithInsertion(people);
+                break;
+            case 4:
+                sortingMethods.sortByNameWithInsertion(people);
+                break;
+            default:
+                view.showMessage("Opción de ordenamiento inválida.");
+        }
+        view.displayPeople(people);
+    }
+
+    private void searchPerson() {
+        if (people == null || people.length == 0) {
+            view.showMessage("No hay personas para buscar.");
+            return;
+        }
+        int searchOption = view.selectSearchCriterion();
+        switch (searchOption) {
+            case 1:
+                int age = view.inputInt("Ingrese la edad a buscar: ");
+                Person resultByAge = searchMethods.binarySearchByAge(people, age);
+                view.displaySearchResult(resultByAge);
+                break;
+            case 2:
+                String name = view.inputString("Ingrese el nombre a buscar: ");
+                Person resultByName = searchMethods.binarySearchByName(people, name);
+                view.displaySearchResult(resultByName);
+                break;
+            default:
+                view.showMessage("Criterio de búsqueda inválido.");
         }
     }
 }
