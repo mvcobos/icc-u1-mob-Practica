fd170cf16fe6913e125ec9d9ef03b985892c4f97
mvcobos <vcobos178@gmail.com>
2024-12-04 22:44:01 -0500

    Update SortingMethods.java

--- src/controllers/SortingMethods.java
+++ src/controllers/SortingMethods.java
@@ -4,21 +4,37 @@ import models.Person;
 
 public class SortingMethods {
 
-    public void sortByNameBubble(Person[] personas) {
-        int n = personas.length;
-        for(int i = 0; i < (n - 1); i++){
-            for(int j = 0 ; j < n - i - 1; j++ ){
-                if(personas[j].getName().compareTo(personas[j+1].getName()) > 0){
-                    Person temp = personas [j];
-                    personas [j] = personas [j + 1];
-                    personas [j + 1] = temp;
-                }
+    public Person binarySearchByAge(Person[] people, int age) {
+        int left = 0, right = people.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (people[mid].getAge() == age) {
+                return people[mid];
             }
-        } 
+            if (people[mid].getAge() < age) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
     }
-
-    public void sortByAgeWhitSelection(Person[] personas) {
-
+    
+    public Person binarySearchByName(Person[] people, String name) {
+        int left = 0, right = people.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = people[mid].getName().compareToIgnoreCase(name);
+            if (comparison == 0) {
+                return people[mid];
+            }
+            if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
     }
 
 }
