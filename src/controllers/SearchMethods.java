be08c790863abcfbd63e902940b49dd77acabe09
mvcobos <vcobos178@gmail.com>
2024-12-04 23:55:48 -0500

    Update SearchMethods.java

--- src/controllers/SearchMethods.java
+++ src/controllers/SearchMethods.java
@@ -1,40 +1,45 @@
 package controllers;
+import models.Person;
 
 public class SearchMethods {
     private boolean sortedByAge = false;
     private boolean sortedByName = false;
 
-    public Person binarySearchByAge(Person[] persons, int age) {
-        int left = 0, right = persons.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (persons[mid].getAge() == age) {
-                return persons[mid];
+    public int binarySearchByAge(Person[] persons, int datoBuscar){
+        int inicio = 0;
+        int fin = persons.length - 1;
+        while (inicio <= fin){
+            int mid = (inicio + fin) / 2;
+            if(persons[mid].getAge() == datoBuscar){
+                return mid;
             }
-            if (persons[mid].getAge() < age) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if(persons[mid].getAge() > datoBuscar){
+                inicio = mid + 1;
+            }
+            if (persons[mid].getAge() < datoBuscar){
+                fin = mid - 1;
             }
         }
-        return null;
+        return -1;
     }
 
-    public Person binarySearchByName(Person[] persons, String name) {
-        int left = 0, right = persons.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int comparison = persons[mid].getName().compareToIgnoreCase(name);
-            if (comparison == 0) {
-                return persons[mid];
+
+    public int binarySearchByName(Person[] persons, String datoBuscar){
+        int inicio = 0;
+        int fin = persons.length - 1;
+        while (inicio <= fin){
+            int mid = (inicio + fin) / 2;
+            if(persons[mid].getName().compareTo(datoBuscar) == 0){
+                return mid;
             }
-            if (comparison < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if(persons[mid].getName().compareTo(datoBuscar) < 0){
+                inicio = mid + 1;
+            }
+            if (persons[mid].getName().compareTo(datoBuscar) > 0){
+                fin = mid - 1;
             }
         }
-        return null;
+        return -1;
     }
 
     public boolean isSortedByAge() {
